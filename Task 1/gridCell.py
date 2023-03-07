37030421942225f82faa480aa700e8a5cffdfd2b
Kylie Greenwald <kg208@duke.edu>
2023-03-06 21:30:45 -0500

    added code for finding hubs

--- Task 1/gridCell.py
+++ Task 1/gridCell.py
@@ -1,21 +1,20 @@
 import csv
 import math
+import queue, heapq
+import matplotlib.pyplot as plt
 
 # to parse file
 def getPoints(file):
     points = []
-    # minimum = 0
-    # maximum = 0
     with open(file, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             points.append([float(row["x"]), float(row["y"])])
-            # maximum = max(maximum, points[-1][0], points[-1][1])
-            # minimum = min(minimum, points[-1][0], points[-1][1])
     return points
 
-# should I standardize this? feed min and max as inputs?
 # pre-processing
+# minimum: -105, maximum: 105 -> 0.25 x 0.25 km grids represented by the matrix 
+# minimum: -420, maximum: 420 -> n x m matrix where n = m = 840
 def densityMatrix(pts):
     denseMatrix = [ [0]*840 for i in range(840) ]
     for point in pts:
@@ -24,10 +23,92 @@ def densityMatrix(pts):
         denseMatrix[x][y] += 1
     return denseMatrix
 
-# full data set: minimum = -103.532808, maximum = 96.237468
-full_pts = getPoints('./geolife-cars.csv')
+# not really a necessary function
+def density(matrix, point):
+    x = math.floor(point[0] * 4) + 419
+    y = math.floor(point[1] * 4) + 419
+    return matrix[x][y]
 
-# mtrx = densityMatrix(full_pts)
+def hubs(matrix, k, radius):
+    PQ = []
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j] != 0:
+                # priority based on negative density in order to sort correctly!
+                heapq.heappush(PQ, (-matrix[i][j], [i, j]))
+                
+    numHubs = 0
+    hubList = []
+    while numHubs < k:
+        density, cell = heapq.heappop(PQ)
+        
+        # convert cell back to standard point
+        xnorm = (cell[0] - 419)/4
+        ynorm = (cell[1] - 419)/4
+        hubList.append([-density, (xnorm, ynorm)])
+        numHubs += 1
+        
+        # calculate bounds
+        xmin = cell[0] - (radius*4)
+        xmax = cell[0] + (radius*4)
+        ymin = cell[1] - (radius*4)
+        ymax = cell[1] + (radius*4)
+        
+        # index
+        i = 0
+        while i < len(PQ):
+            x = PQ[i][1][0]
+            y = PQ[i][1][1]
+            if x >= xmin and x <= xmax and y >= ymin and y <= ymax:
+                PQ.pop(i)
+            i = i + 1
+        heapq.heapify(PQ)
+    return hubList
+    
+
+### INPUT ###
+full_pts = getPoints('./geolife-cars.csv') # minimum = -103.532808, maximum = 96.237468
+# ten_pts, minimum, maximum = getPoints('./geolife-cars-ten-percent.csv')
+# thirty_pts = getPoints('./geolife-cars-thirty-percent.csv')
+# sixty_pts = getPoints('./geolife-cars-sixty-percent.csv')
+
+matrix = densityMatrix(full_pts)
+print(hubs(matrix, 10, 10))
+
+# PLOTTING
+# plt.scatter([item[0] for item in full_pts], [item[1] for item in full_pts], c = "blue", s = 0.01)
+# plt.show()
+
+# def density(point, matrix, radius):
+#     x = math.floor(point[0] * 4) + 419
+#     y = math.floor(point[1] * 4) + 419
+#     xmin = (x - radius) * 4
+#     xmax = (x + radius) * 4
+#     ymin = (y - radius) * 4
+#     ymax = (y + radius) * 4
+#     count = 0
+#     for i in range(xmin, xmax):
+#         for j in range(ymin, ymax):
+#             if compute_distance([[i, j], point])
+#     return matrix[x][y]
+
+# x, y = p
+#     xmin = max(0, x - radius)
+#     xmax = min(matrix.shape[0], x + radius + 1)
+#     ymin = max(0, y - radius)
+#     ymax = min(matrix.shape[1], y + radius + 1)
+
+#     count = 0
+#     for i in range(xmin, xmax):
+#         for j in range(ymin, ymax):
+#             if ((i - x) ** 2 + (j - y) ** 2) ** 0.5 <= radius:
+#                 count += matrix[i, j]
+
+#     area = (2 * radius + 1) ** 2
+#     return count / area
+
+# matrix = densityMatrix(full_pts)
+# print(matrix)
 
 # Algorithm
 # iterate through all the points, check if a point is within a certain cell
